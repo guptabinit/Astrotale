c78b48060c0a51cfb675162ea778d914943e197d
Binit Gupta <binitguptapro@gmail.com>
2023-07-06 12:32:13 +0530

    init

---
+++ android/app/src/main/kotlin/com/astrotale/astrotale/astrotale/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.astrotale.astrotale.astrotale
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xff621EB0);
+const backgroundColor = Color(0xffffffff);
+
+const darkColor = Color(0xff292D32);
+const greyColor = Color(0xff808385);
+const lightColor = Colors.white;
